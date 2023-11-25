827afb4b419ac6cc74642117fc900d0a9faea489
x10101 <elaine9210@gmail.com>
2023-11-25 14:26:03 +0800

    update 1a2b.js

--- 1a2b.js
+++ 1a2b.js
@@ -1,18 +1,31 @@
-var play = true;
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-while (play) {
-    const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+function play() {
     var [a, b, c, d] = numbers.sort(() => Math.random() - 0.5);
     var ans = [a, b, c, d];
     var A = 0;
     var B = 0;
     var gussTime = 0; // 猜的次數
-    var guss = "";
+    var guess = [];
 
-    
-}
+    // 將使用者按的數字按鈕加入變數
+    function inputNumber(num) {
+        guess.push(num);
+        showNumber();
+    }
 
-function setValue() {
-    var input = document.getElementById("customInput");
-    guss = input.setValue;
+    // 更新輸入框中的數值
+    function showNumber() {
+        document.getElementById("customInput").value = guess.join("");
+    }
+
+    // 刪除輸入的數值
+    function deleteValue() {
+        guess = [];
+        showNumber();
+    }
+
+    function submitValue() {
+        
+    }
 }
