39af7e496b2b5f86f6760697f212a5cf378c32f0
Annie <annielee197988@gmail.com>
2023-11-25 16:21:04 +0800

    all button hide

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
 /*按鈕隱藏*/
 function button_hide(){
-    var Button = document.getElementById("button1");
-    Button.style.visibility = "hidden";
+    var Button1 = document.getElementById("button1");
+    Button1.style.visibility = "hidden";
+
+    var Button2 = document.getElementById("button2");
+    Button2.style.visibility = "hidden";
+
+    var Button3 = document.getElementById("button3");
+    Button3.style.visibility = "hidden";
+
+    var Button3 = document.getElementById("button3");
+    Button3.style.visibility = "hidden";
+
+    var Button4 = document.getElementById("button4");
+    Button4.style.visibility = "hidden";
+
+    var Button5 = document.getElementById("button5");
+    Button5.style.visibility = "hidden";
+
+    var Button6 = document.getElementById("button6");
+    Button6.style.visibility = "hidden";
+
+    var Button7 = document.getElementById("button7");
+    Button7.style.visibility = "hidden";
+
+    var Button8 = document.getElementById("button8");
+    Button8.style.visibility = "hidden";
+
+    var Button9 = document.getElementById("button9");
+    Button9.style.visibility = "hidden";
 }
 
 /*恢復按鈕*/
+function button_recover(){
+    var Delete = document.getElementById("delete");
+    if(Button_recover.style.visibility == "hidden"){
+        Button_recover.style.visibility = "visible";
+    }
+}
