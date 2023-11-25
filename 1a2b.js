262cae6c23826c665af5da823aa6b1beca391230
x10101 <elaine9210@gmail.com>
2023-11-25 13:47:08 +0800

    更新成setValue()

--- 1a2b.js
+++ 1a2b.js
@@ -1,11 +1,15 @@
-const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-let [a, b, c, d] = numbers.sort(() => Math.random() - 0.5);
-let ans = [a, b, c, d];
-let A = 0;
-let B = 0;
-let gussTime = 0; // 猜的次數
+var play = true;
 
-function getValue(){
-    var guss = document.getElementById("customInput");
-    alert(guss.value);
+while (play) {
+    const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    var [a, b, c, d] = numbers.sort(() => Math.random() - 0.5);
+    var ans = [a, b, c, d];
+    var A = 0;
+    var B = 0;
+    var gussTime = 0; // 猜的次數
+
+    function setValue(){
+        var guss = document.getElementById("customInput");
+        alert(guss.value);
+    }
 }
