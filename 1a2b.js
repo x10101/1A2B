12532ea813d2b086731f44e0f84b85a2ea1a3dff
x10101 <elaine9210@gmail.com>
2023-11-25 22:06:59 +0800

    調整送出訊息

--- 1a2b.js
+++ 1a2b.js
@@ -1,77 +1,79 @@
 const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-//function play() {
+// 開始遊戲，重設變數
+function play() {
     var [a, b, c, d] = numbers.sort(() => Math.random() - 0.5);
     var ans = [a, b, c, d];
     var A = 0;
     var B = 0;
     var gussTime = 0; // 猜的次數
     var guess = [];
+}
 
-    // 將使用者按的數字按鈕加入變數
-    function inputNumber(num) {
-        if (guess.length >= 4) {
-            alert("一次只能輸入四個數字！");
-        }
-        else {
-            guess.push(num);
-            showNumber();
-        }
+// 將使用者按的數字按鈕加入變數
+function inputNumber(num) {
+    if (guess.length >= 4) {
+        alert("一次只能輸入四個數字！");
     }
-
-    // 更新輸入框中的數值
-    function showNumber() {
-        let n = guess.join("");
-        document.getElementById("customInput").textContent = n;
-    }
-
-    // 刪除輸入的數值
-    function deleteValue() {
-        guess = [];
+    else {
+        guess.push(num);
         showNumber();
     }
+}
 
-    // 送出數值，判斷與顯示結果
-    function submitValue() {
-        if (guess < 4) alert("要輸入四個數字");
-        else {
-            gussTime++;
+// 更新輸入框中的數值
+function showNumber() {
+    let n = guess.join("");
+    document.getElementById("customInput").textContent = n;
+}
 
-            let userMessage = document.createElement("div");
-            let text = document.createElement("span");
-            text.textContent = guess.join("");
-            userMessage.appendChild(text);
-            userMessage.classList.add("player");
-            document.getElementById("chatBox").appendChild(userMessage);
-            for (i = 0; i < 4; i++) {
-                for (j = 0; j < 4; j++) {
-                    if (ans[i] == guess[j]) {
-                        if (i == j) A++;
-                        else B++;
-                    }
+// 刪除輸入的數值
+function deleteValue() {
+    guess = [];
+    showNumber();
+}
+
+// 送出數值，判斷與顯示結果
+function submitValue() {
+    if (guess < 4) alert("要輸入四個數字");
+    else {
+        gussTime++;
+        for (i = 0; i < 4; i++) {
+            for (j = 0; j < 4; j++) {
+                if (ans[i] == guess[j]) {
+                    if (i == j) A++;
+                    else B++;
                 }
             }
+        }
+        sendMessage("player", guess.join(""));
 
-            if (A != 4) {
-                let botMessage = document.createElement("div");
-                let text = document.createElement("span");
-                text.textContent = [A,"A",B,"B"].join("");
-                botMessage.appendChild(text);
-                botMessage.classList.add("bot");
-                document.getElementById("chatBox").appendChild(botMessage);
-                A = 0;
-                B = 0;
-            }
-            else {
-                let botMessage = document.createElement("div");
-                let text = document.createElement("span");
-                text.textContent = "恭喜答對！共猜了" + gussTime + "次";
-                botMessage.appendChild(text);
-                botMessage.classList.add("bot");
-                document.getElementById("chatBox").appendChild(botMessage);
-            }
-            deleteValue();
-            chatBox.scrollTop = chatBox.scrollHeight;
+        if (A != 4) {
+            sendMessage("bot", [A,"A",B,"B"].join(""));
+            A = 0;
+            B = 0;
+        }
+        else {
+            sendMessage("bot", "恭喜答對！共猜了" + gussTime + "次");
+            
+            document.getElementById("endGame").style.visibility = "visible";
         }
     }
-//}
+}
+
+// 送出bot或player訊息
+function sendMessage(type, mes) {
+    let Message = document.createElement("div");
+    let text = document.createElement("span");
+    text.textContent = mes;
+    Message.appendChild(text);
+    Message.classList.add(type);
+    document.getElementById("chatBox").appendChild(Message);
+    chatBox.scrollTop = chatBox.scrollHeight;
+
+    deleteValue();
+}
+
+function end() {
+
+}
