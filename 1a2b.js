6b4c3518a64c3a8918c740d9fe6dce36a16a3c7f
x10101 <elaine9210@gmail.com>
2023-11-25 13:26:16 +0800

    新增標籤圖示、刪除多於邊界、add 1a2b.js

---
+++ 1a2b.js
@@ -0,0 +1,9 @@
+const numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let [a, b, c, d] = numbers.sort(() => Math.random() - 0.5);
+let ans = [a, b, c, d];
+let A = 0;
+let B = 0;
+let gussTime = 0; // 猜的次數
+
+var guss = document.getElementById;
+
