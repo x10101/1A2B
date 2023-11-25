4a2e670b2ab6f0c8830837fe53a234bb00f95b79
Annie <annielee197988@gmail.com>
2023-11-25 14:37:46 +0800

    class->id

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /*按鈕隱藏*/
 function button_hide(){
-    var Button1 = document.getElementsByClassName("button1");
+    var Button1 = document.getElementsById("button1");
     Button1.style.visibility = "hidden";
 }
 
