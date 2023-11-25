9dbf604680402f01d606658cf6cfeb64bf8dce4c
Annie <annielee197988@gmail.com>
2023-11-25 14:09:35 +0800

    try hide 6

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*按鈕隱藏*/
 function button_hide(){
     var Button1 = document.getElementsByClassName(button1);
-    Button1.style.visibility = "none";
+    Button1.style.visibility = "hidden";
 }
 
 /*恢復按鈕*/
